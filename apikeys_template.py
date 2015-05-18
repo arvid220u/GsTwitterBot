1aef42564fa5552b6889d15db89f3fa5504f5caa
ArVID220u <arvid.lunnemark@gmail.com>
2015-05-18 16:15:54 +0200

    Adding a template for the apikeys file.

---
+++ apikeys_template.py
@@ -0,0 +1,6 @@
+# The Twitter API keys needed to send tweets
+
+CONSUMER_KEY = "enter your consumer key here"
+CONSUMER_SECRET = "enter your secret consumer key here"
+ACCESS_TOKEN = "enter your access token here"
+ACCESS_TOKEN_SECRET = "enter your secret access token here"
