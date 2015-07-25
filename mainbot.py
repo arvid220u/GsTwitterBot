79881b3809d3f1e482e56c3d56da99078d1dc530
ArVID220u <arvid.lunnemark@gmail.com>
2015-07-25 21:29:05 +0200

    Error handling fixed to work; still ugly though

--- mainbot.py
+++ mainbot.py
@@ -12,6 +12,8 @@ from fastreplystreamer import FastReplyStreamer
 import apikeys
 # import Thread to be able to run concurrently
 from threading import Thread
+# randint for the tweet interval
+from random import randint
 
 
 
@@ -19,35 +21,18 @@ from threading import Thread
 # the bash command "python3 mainbot.py" will call this function
 def main():
 
-    # make error handling simple:
-    # if there's any error anywhere, catch it at this top level
-    # this makes the code not spoilt with try/except clauses everywhere
-    # however, it also catches keyboardinterrupt and type errors
-    # if error handling is needed at a deeper level somewhere, that is possible
-    # wrap it all in a while true loop, so the program will start over at any errors
-    while True:
-        try:
+    # first setup users and markov objects
+    setup()
+    
+    # create two threads, which will call reply_streamer and tweet_loop
+    # use threads to make these threads be able to run concurrently
+    reply_streamer_thread = Thread(target = reply_streamer)
+    tweet_loop_thread = Thread(target = tweet_loop)
+    # start both threads
+    reply_streamer_thread.start()
+    tweet_loop_thread.start()
+    # these threads will run infinitely
 
-            # first setup users and markov objects
-            setup()
-            
-            # create two threads, which will call reply_streamer and tweet_loop
-            # use threads to make these threads be able to run concurrently
-            reply_streamer_thread = Thread(target = reply_streamer)
-            tweet_loop_thread = Thread(target = tweet_loop)
-            # start both threads
-            reply_streamer_thread.start()
-            tweet_loop_thread.start()
-            # these threads will run infinitely
-
-        except Exception as exception:
-            # print the exception
-            print(exception)
-
-            # wait for 1 hour, so as to avoid rate limiting, or servers down
-            time.sleep(60*60)
-            # now program will start over
-            print("Restarting program...")
 
 
 
@@ -97,8 +82,18 @@ def reply_streamer():
     # pass this markov instance to the streamer, so it will be able to generate replies
     streamer.markov = markov
     # start the filter
-    streamer.statuses.filter(track=("@" + twythonaccess.screen_name))
-    print("finished registering for streaming api")
+    # nest it in error handling
+    while True:
+        try:
+            streamer.statuses.filter(track=("@" + twythonaccess.screen_name))
+        except Exception as exception:
+            # print the exception and then sleep for an hour
+            # the sleep will reset rate limit
+            # if twitter's servers were down, they may be up after the sleep
+            # after the sleep, the filter function will be called anew
+            print(exception)
+            time.sleep(60*60)
+            print("finished sleep after exception in streaming api. will now start anew")
 
 
 # the run loop, which will continue in infinity
@@ -106,78 +101,94 @@ def tweet_loop():
     while True:
         print("start loop")
 
-        # get a tweet
-        #tweet = markov.generate_tweet()
+        
+        # the try is for error handling
+        # if any of the twython methods (nested or not) raise an exception,
+        # then it will be caught in this except clause
+        # the except clause will do nothing but sleep for a while,
+        # and then continue with the loop
+        try:
 
-        # send tweet
-        while True:
-            # generate new tweet
-            tweet = markov.generate_tweet()
-            print("tweet generated: " + tweet)
-            if twythonaccess.send_tweet(tweet):
-                # the generated tweet is okay
-                print("tweet approved or passed")
-                break
+            # send tweet
+            while True:
+                # generate new tweet
+                tweet = markov.generate_tweet()
+                print("tweet generated: " + tweet)
+                if twythonaccess.send_tweet(tweet):
+                    # the generated tweet is okay
+                    print("tweet approved or passed")
+                    break
 
-        # sleeping point, if need for sleep
-        # sleep for one day
-        # make the sleep somewhat randomized
-        # can range from 5 minutes to 3 days
-        # make the distribution curve peak at 24 hours
-        # get a random integer between 5 and 2.25*24*60, representing minutes
-        # based on calculations, do first loop 5 times and second loop once
-        min_sleep = 5
-        max_sleep = 2.25*24*60
-        sleep_minutes = randint(min_sleep, max_sleep)
-        for i in range(0,4):
-            # if the value isn't in the specified range, then regenerate the value
-            # this will increase the statistical probability of the value falling in the range, while not limiting the value directly
-            # first limit the value in a close range, then in a bigger range, order is important
-            # this will make the curve like a double-sided stairway with three levels, the middle level being the widest
-            if sleep_minutes < 23*60 or sleep_minutes > 27*60:
-                sleep_minutes = randint(min_sleep, max_sleep)
-        # second loop, wider, designed to catch almost all values
-        for i in range(0,1):
-            if sleep_minutes < 5*60 or sleep_minutes > 48*60:
-                sleep_minutes = randint(min_sleep, max_sleep)
+            # sleeping point, if need for sleep
+            # sleep for one day
+            # make the sleep somewhat randomized
+            # can range from 5 minutes to 3 days
+            # make the distribution curve peak at 24 hours
+            # get a random integer between 5 and 2.25*24*60, representing minutes
+            # based on calculations, do first loop 5 times and second loop once
+            min_sleep = 5
+            max_sleep = 2.25*24*60
+            sleep_minutes = randint(min_sleep, max_sleep)
+            for i in range(0,4):
+                # if the value isn't in the specified range, then regenerate the value
+                # this will increase the statistical probability of the value falling in the range, while not limiting the value directly
+                # first limit the value in a close range, then in a bigger range, order is important
+                # this will make the curve like a double-sided stairway with three levels, the middle level being the widest
+                if sleep_minutes < 23*60 or sleep_minutes > 27*60:
+                    sleep_minutes = randint(min_sleep, max_sleep)
+            # second loop, wider, designed to catch almost all values
+            for i in range(0,1):
+                if sleep_minutes < 6*60 or sleep_minutes > 46*60:
+                    sleep_minutes = randint(min_sleep, max_sleep)
 
-        print("will sleep for " + str(sleep_minutes) + " minutes")
-        time.sleep(sleep_minutes * 60)
-        print("has slept for " + str(sleep_minutes) + " minutes")
+            print("will sleep for " + str(sleep_minutes) + " minutes")
+            time.sleep(sleep_minutes * 60)
+            print("has slept for " + str(sleep_minutes) + " minutes")
+            
+            # temporary sleep
+            time.sleep(60*60)
 
-        # update the users followers
-        users.check_new_followers()
-        print("updated followers")
+            # update the users followers
+            users.check_new_followers()
+            print("updated followers")
 
-        #print("followers:")
-        #for follower in users.followers:
-            #print(follower["screen_name"])
-            #time.sleep(1)
-        #print "followfollowers:"
-        #for followfollower in users.followfollowers:
-            #print(followfollower["screen_name"])
-            #time.sleep(1)
+            #print("followers:")
+            #for follower in users.followers:
+                #print(follower["screen_name"])
+                #time.sleep(1)
+            #print "followfollowers:"
+            #for followfollower in users.followfollowers:
+                #print(followfollower["screen_name"])
+                #time.sleep(1)
 
-        # update the tweets
-        users.check_new_tweets()
-        print("updated tweets")
+            # update the tweets
+            users.check_new_tweets()
+            print("updated tweets")
 
-        #print("follower's tweets:")
-        #for follower_tweet in users.followers_tweets:
-            #print(follower_tweet["text"])
-            #time.sleep(1)
+            #print("follower's tweets:")
+            #for follower_tweet in users.followers_tweets:
+                #print(follower_tweet["text"])
+                #time.sleep(1)
 
-        # get the rossana and goran tweets
-        ghAndRd = ["goranhagglund", "rossa_d"]
-        ghAndRdTweets = []
-        for id in ghAndRd:
-            twythonaccess.check_if_requests_are_maximum(170)
-            this_users_tweets = twythonaccess.authorize().get_user_timeline(screen_name=id, trim_user=True, include_rts=False)
-            ghAndRdTweets.extend(this_users_tweets)
+            # get the rossana and goran tweets
+            ghAndRd = ["goranhagglund", "rossa_d"]
+            ghAndRdTweets = []
+            for id in ghAndRd:
+                twythonaccess.check_if_requests_are_maximum(170)
+                this_users_tweets = twythonaccess.authorize().get_user_timeline(screen_name=id, trim_user=True, include_rts=False)
+                ghAndRdTweets.extend(this_users_tweets)
 
-        # update the markov
-        markov.update_markov(users.followers_tweets + ghAndRdTweets + users.followfollowers_tweets)
-        print("markov updated")
+            # update the markov
+            markov.update_markov(users.followers_tweets + ghAndRdTweets + users.followfollowers_tweets)
+            print("markov updated")
+
+        except Exception as exception:
+            # print out the exception, and then sleep for 1 hour
+            # the exception may be a rate limit, or it may be due to twitter's servers being down
+            # either way, a sleep will help
+            print(exception)
+            time.sleep(60*60)
+            print("finished exception sleep; will resume tweet generation loop as normal")
 
 
 
